60bd660bb9ea49b8ce8b5c7a5ccb96846f796b7f
Larsson Fernández <lfernandezhuaringa@gmail.com>
2025-12-31 09:57:46 -0500

    Segundo avance: Personalización de rutas y paso de variables

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/bienvenida', function () {
+    $nombre = "Larsson Fernandez";
+    $lider = "Arnie Jeampool";
+    return view('welcome', ['usuario' => $nombre, 'jefe' => $lider]);
+});
